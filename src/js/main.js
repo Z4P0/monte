2d29845c6720fe7f78606ae994eb47b7c3027897
luis <lrosario101@gmail.com>
2014-08-08 01:41:10 -0400

    minor edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 'use strict';
 var mtn = mtn || {};
 
-window.onload = mtn.main.init;
-
 
 mtn.main = {
   init: function() {
     console.log('hello from: main.js');
   }
 };
+
+
+window.onload = mtn.main.init;
