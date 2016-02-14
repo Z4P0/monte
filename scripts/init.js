331f156ee9ea13ccaebe3cdc8cb71c7050017ecc
luis rosario <lrosario101@gmail.com>
2016-02-14 05:00:17 -0500

    initialized foundation js

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,38 @@
 var Monte = require('./monte');
+var $ = require('jquery');
+var foundation = require('foundation');
 
-window.addEventListener('DOMContentLoaded', function (e) {
+// initialize the things
+$(document).ready(function () {
+
+  var $body = $('html,body');
+
   // say hi
-  window.monte = new Monte();
+  var monte = new Monte();
   if (console !== undefined) console.log(monte.tag);
 
-  // new things...
-  // --
+  $(document).foundation();
+  monte.init();
+
+  /**
+   * smooth scroll to a section of the page
+   * @return false
+   *
+   * smooth scroll - original source below
+   * http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+  */
+  $('a[data-smooth-scroll]').on('click.smooth_scroll', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $body.animate({
+          scrollTop: target.offset().top
+        }, 1000);
+
+        return false;
+      }
+    }
+  });
 });
+
--- scripts/monte.js
+++ scripts/monte.js
@@ -3,6 +3,12 @@ var settings = {
 }
 
 module.exports = function () {
+
+
   this.tag = 'z a p o';
   this.settings = settings;
+  this.init = function () {
+    // console.log('test');
+  }
+
 }
